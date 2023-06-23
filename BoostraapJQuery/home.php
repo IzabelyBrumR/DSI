dc8705ba693a88cbda8b449ef8812154e550c4e0
IzabelyBrumR <103642004+IzabelyBrumR@users.noreply.github.com>
2023-06-23 10:02:39 -0300

    Add files via upload

---
+++ BoostraapJQuery/home.php
@@ -0,0 +1,21 @@
+<?php
+require_once './shared/header.php';
+?>
+<div class="row">
+    <div class="col-md-4"></div>
+    <div class="row" style="border: 2px solid green">
+        <div class="mb-3 mt-3">
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Insira seu email:" name="email">
+        </div>
+        <div class="mb-3 mt-3">
+            <label for="senha" class="form-label">Senha:</label>
+            <input type="password" class="form-control" id="senha" 
+                   placeholder="Insira sua senha:" name="senha">
+        </div>
+    </div>
+    <div class="col-md-4"></div>
+</div>
+<?php
+require_once './shared/footer.php';
+?>
